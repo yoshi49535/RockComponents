86330943d51187a4d2cbdc0197175bc13fafe1ab
Yoshi <yoshi@44services.jp>
2012-05-11 04:10:49 +0900

    add new Exception

---
+++ src/Flow/Exception/FlowHandleException.php
@@ -0,0 +1,26 @@
+<?php
+/****
+ *
+ * Description:
+ *      
+ * $Id$
+ * $Date$
+ * $Rev$
+ * $Author$
+ * 
+ *  This file is part of the $Project$ package.
+ *
+ * $Copyrights$
+ *
+ ****/
+// @namespace
+namespace Rock\Component\Flow\Exception;
+// @extends Flow Interface
+use Rock\Component\Flow\Exception\FlowRuntimeException;
+
+/**
+ *
+ */
+class FlowHandleException extends FlowRuntimeException
+{
+}
